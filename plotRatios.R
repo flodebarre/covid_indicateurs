d54bbe838c6c0ccbb3ec8cf8b167327b585ee37f
flodebarre <florence.debarre@normalesup.org>
2022-03-27 20:23:22 +0200

    add comments

--- plotRatios.R
+++ plotRatios.R
@@ -1,5 +1,9 @@
-dlData <- FALSE
-source("usefulFunctions.R") # Sliding average function
+
+# Initializations ####
+
+dlData <- TRUE # Whether to download the data
+
+source("usefulFunctions.R") # Load sliding average function
 
 # Load data ####
 
@@ -13,16 +17,17 @@ if(dlData){
 
 dat <- read.csv(dataFile2, stringsAsFactors = FALSE)
 dat$date <- as.Date(dat$date)
-tail(dat, 10)
 
-# Checks
+tail(dat, 10) # Check the dates
+
+# Check that all dates are consecutive (necessary to compute ratios)
 stopifnot(all(diff(dat$date) == 1))
 
-# Load population size used by SPF
+# Load population size used by SPF to convert case numbers to incidence per 100'000
 pops <- read.csv("../data/SPFpopFra2020_90.csv")
 popFR <- pops[pops$clage_vacsi == 0, "pop"]
 
-# Load jours feries
+# Load holidays (jours feries), to identify days on which data can be unusual
 feries <- read.csv("data/jours_feries_metropole.csv")
 feries$date <- as.Date(feries$date)
 dat$ferie <- is.element(dat$date, feries$date)
@@ -30,17 +35,19 @@ dat$ferie <- is.element(dat$date, feries$date)
 #........................................................................................
 # Compute ratios etc. ####
 n1 <- nrow(dat)
-n2 <- n1 - 3
+n2 <- n1 - 3 # Max number of values of cases by test date
 
 # Compute positive cases on date of result (need a 1-day offset)
 dat$P_dateResult <- c(tail(dat$conf_j1, -1), NA)
 # Rename positive results on date of test
 dat$P_dateTest <- dat$pos
 
-# Remove lines for jours feries
+# Set lines for jours feries to NA
 dat[which(dat$ferie), c("P_dateResult", "P_dateTest")] <- NA
 
 # Compute 7-d incidence; center at the middle of the interval
+# (7 * mean instead of sum to account for NAs)
+# (restricted to 1:n2, where we have data)
 dat$incid7J <- c(7 * sliding.window(dat$P_dateTest[1:n2], FUN = mean) / popFR * 10^5, rep(NA, 3))
 
 # Compute ratios for the three types of case numbers
@@ -53,35 +60,36 @@ dat$ratio7_dateTest[8:n2] <- dat$P_dateTest[8:n2] / dat$P_dateTest[8:n2 - 7]
 dat$ratio7_incid <- NA
 dat$ratio7_incid[8:n1] <- dat$incid7J[8:n1] / dat$incid7J[8:n1 - 7]
 
-# Remove the last line
+# Remove the last line because we do not have case data on the last day
 dat <- head(dat, -1)
 
+# Plot settings
 # Colors chosen using Paletton 
 # https://paletton.com/#uid=30a0u0kprrvfFAukHtUsrmuw4hF
 colCroiss <- "#DB512D"
 colDecroiss <- "#22698B"
 
-tail(dat, 10)
 par(las = 1)
 
-# Lines
+# Define data frame to prepare the horizontal lines of doubling times
 doublings <- data.frame(times = c(1, 2, 4, 10/7, 5/7),
                         namesFR = c("1 semaine", "2 semaines", "4 semaines", "10 jours", "5 jours"))
 
-minDate <- as.Date("2021-12-15")
-maxDate <- max(dat$date)
-ylm <- c(0.5, 3)
-lang <- "FR"
+minDate <- as.Date("2021-12-15") # minimum date in the plot
+maxDate <- max(dat$date) # max date in the plot
+ylm <- c(0.5, 3) # y range
+lang <- "FR" # Language (EN is work in progress)
 
-fname <- "pics/ratiosFrance.png"
+fname <- "pics/ratiosFrance.png" # output name
 png(filename = fname, width = 10.66, height = 6, units = "in", res = 300)
 
 par(mgp = c(2.5, 0.5, 0.5), las = 1, 
     mar = c(3, 8, 3, 8), 
     cex = 1)
+# Initialize plot figure
 plot(dat$date, dat$ratio7_dateTest, pch = 16, xlim = c(minDate, maxDate), ylim = ylm, log = "y", type = "n", axes = FALSE, xlab = "", ylab = "")
 
-# Initialize figure
+# Axes labels
 txt <- ifelse(lang == "FR", 
               "cas(j)/cas(j-7), échelle log", 
               "cases(j)/cases(j-7), log scale")
@@ -96,10 +104,12 @@ op <- 0.25 # Opacity parameter
 # Add Shading
 # Decroissance
 polygon(x = as.Date(c(limPlot[1], limPlot[2], limPlot[2], limPlot[1], limPlot[1]), origin = "1970-01-01"), 
-        y = c(1, 1, ylm[1], ylm[1], 1), border = NA, col = adjustcolor(colDecroiss, op))
+        y = c(1, 1, ylm[1], ylm[1], 1), 
+        border = NA, col = adjustcolor(colDecroiss, op))
 # Croissance
 polygon(x = as.Date(c(limPlot[1], limPlot[2], limPlot[2], limPlot[1], limPlot[1]), origin = "1970-01-01"), 
-        y = c(1, 1, ylm[2], ylm[2], 1), border = NA, col = adjustcolor(colCroiss, op))
+        y = c(1, 1, ylm[2], ylm[2], 1), 
+        border = NA, col = adjustcolor(colCroiss, op))
 
 # Add horizontal lines for doubling times
 par(xpd = FALSE)
@@ -110,9 +120,11 @@ lwdd <- 1.5 # Line width
 dxx <- 1 # offset for labels
 cexx <- 1/1.2
 for(tt in doublings$times){
+  # Horizontal lines for x and / doubling/halving times
   abline(h = exp(log(2) / tt), col = colline, lwd = lwdd)
   abline(h = exp(-log(2) / tt), col = colline, lwd = lwdd)
   
+  # Add text legend
   par(xpd = TRUE)
   text(limPlot[2] + dxx, y = exp(log(2) / tt), labels = doublings[doublings$times == tt, "namesFR"], col = colCroiss, adj = 0, cex = cexx)
   if(tt >= 1){
@@ -121,11 +133,11 @@ for(tt in doublings$times){
   par(xpd = FALSE)
 }
 
+# Add titles for the two halves
 par(xpd = TRUE)
 text(limPlot[2] + dxx, y = exp(log(2) / 8), labels = "Cas x2 en...", adj = 0, cex = 1.2 * cexx, col = colCroiss)
 text(limPlot[2] + dxx, y = exp(- log(2) / 8), labels = "Cas ÷2 en...", adj = 0, cex = 1.2 * cexx, col = colDecroiss)
 par(xpd = FALSE)
-
 
 # Add mention direction in shaded areas
 cexlabshade <- 0.9
@@ -141,8 +153,8 @@ text(as.Date((limPlot[1]+limPlot[2])/2, origin = "1970-01-01"), ylm[2], adj = c(
 
 
 # Add axes
-tckk <- -0.02
-cexx <- 0.8
+tckk <- -0.02 # Ticks
+cexx <- 0.8 # Font size
 days <- seq(as.Date("2020-01-01"), as.Date(maxDate)+30, by = "day")
 dateFormat <- "%d/%m"
 adj <- 0.5
@@ -153,16 +165,16 @@ mnths <- seq(as.Date("2020-01-01"), as.Date(maxDate), by = "month")
 axis(1, at = mnths, labels = format(mnths, format = dateFormat), las = 1, cex.axis = cexx, tck = tckk, lwd = 0, lwd.ticks = 1, pos = ylm[1], adj = adj)
 axis(1, at = mnths + 14, labels = format(mnths + 14, format = dateFormat), las = 1, cex.axis = cexx, tck = tckk, lwd = 0, lwd.ticks = 1, pos = ylm[1], adj = adj)
 
-
 axis(2, at = c(seq(0.5, 1, by = 0.1), seq(1.2, 3, by = 0.2)), tck = -.01, pos = as.Date(limPlot[1], origin = "1970-01-01"), cex.axis = cexx)
 
+# Plot parameters for the points and lines (Test and Res)
 pchTest <- 16
 pchRes <- 1
 
 colTest <- gray(0)
 colRes <- gray(0.5)
 
-# Curve for average
+# Curve for 7-d average of the ratios
 opC <- 0.7
 lines(dat$date[1:n2], sliding.window(dat$ratio7_dateTest[1:n2]), col = adjustcolor(colTest, opC))
 lines(dat$date, sliding.window(dat$ratio7_dateRes), col = adjustcolor(colRes, opC))
@@ -171,12 +183,13 @@ lines(dat$date, sliding.window(dat$ratio7_dateRes), col = adjustcolor(colRes, op
 points(dat$date, dat$ratio7_dateTest, pch = pchTest, col = colTest)
 points(dat$date, dat$ratio7_dateRes, pch = pchRes, col = colRes, cex = 1.1)
 
+# Legend of the points
 legend(as.Date((limPlot[1]+limPlot[2])/2, origin = "1970-01-01"), ylm[2], horiz = TRUE, 
        pch = c(pchTest, pchRes), 
        col = c(colTest, colRes), 
        legend = c("en date de prélévement", "en date de résultat"), yjust = 0.1, xjust = 0.5, box.col = gray(0, 0), box.lwd = 0, pt.cex = 1, cex = cexx, bty = "n")
 
-# Title
+# Title of the plot
 txt <- ifelse(lang == "FR", 
               "Ratio du nombre de cas Covid-19 détectés en France 
 d'une semaine à l'autre, jusqu'au ", 
